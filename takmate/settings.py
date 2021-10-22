88523109b23891e6b107c3e021d6d185ea37c319
arunyadav <arunyadav96392@gmail.com>
2021-10-22 21:50:09 +0530

    uploading code

--- takmate/settings.py
+++ takmate/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
+import django_heroku
 import os
 import environ
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -120,7 +120,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
@@ -128,3 +128,5 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'list'
 LOGIN_URL = 'login'
+
+django_heroku.settings(locals())
